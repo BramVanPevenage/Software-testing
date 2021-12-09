90fa1bb6338f4b569254d205405d23ee29d706a5
BramVanPevenage <55691795+BramVanPevenage@users.noreply.github.com>
2021-12-09 09:56:56 +0100

    update java db testing

--- demoTastDb/src/test/java/com/example/demotastdb/DemoTastDbApplicationTests.java
+++ demoTastDb/src/test/java/com/example/demotastdb/DemoTastDbApplicationTests.java
@@ -4,9 +4,7 @@ import com.example.demotastdb.controller.ProductController;
 import com.example.demotastdb.entity.Product;
 import com.example.demotastdb.service.ProductService;
 import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,8 +12,11 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -45,11 +46,51 @@ class DemoTastDbApplicationTests {
         p3 = new Product("P3", "Updated Dummy Product",300);
     }
 
-    @Test
-    void findAll_6() {
-        Mockito.when(productService.findAll()).thenReturn(Arrays.asList());
-        assertEquals(6,productController.getAll().size());
-        Mockito.verify(productService, Mockito.times(1)).findAll();
+    //Controller Unit Testing
+    @Nested
+    @DisplayName("Testing ProductController")
+    @Tag("ProductControllerUnitTests")
+    class ControllerTests{
+        @Test
+        @DisplayName("No record")
+        void findAll_WhenNoRecord() {
+            Mockito.when(productService.findAll()).thenReturn(Arrays.asList());
+            assertEquals(0,productController.getAll().size());
+            Mockito.verify(productService, Mockito.times(1)).findAll();
+        }
+
+        @Test
+        @DisplayName("Two records")
+        void findAll_WhenRecord() {
+            Mockito.when(productService.findAll()).thenReturn(Arrays.asList(p1,p2));
+            assertEquals(2,productController.getAll().size());
+            Mockito.verify(productService, Mockito.times(1)).findAll();//Testen of we wel de service aanroepen
+        }
+
+        @Test
+        @DisplayName("Create record")
+        void create() {
+            ResponseEntity<Product> p = productController.create(p1);
+            Mockito.verify(productService, Mockito.times(1)).createOrUpdate(p1);
+        }
+
+        @Test
+        @DisplayName("Find id record succeed")
+        void findById_WhenMatch() {
+
+            Mockito.when(productService.findById(1L)).thenReturn(Optional.of(p1));
+            ResponseEntity<Product> p = productController.findById(1L);
+            assertEquals(p.getBody(), p1 );
+        }
+
+        @Test()
+        @DisplayName("Find id record failed")
+        void findById_WhenNoMatch() {
+
+            Mockito.when(productService.findById(1L)).thenReturn(Optional.empty());
+            ResponseEntity<Product> p = productController.findById(1L);
+            assertEquals(p.getStatusCode(), HttpStatus.NOT_FOUND);
+        }
     }
 
 }
